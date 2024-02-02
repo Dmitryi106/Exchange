87c90daa5800efa36964d59c4a98e98c3d2e3ca9
Dmitryi <ilacker106@gmail.com>
2024-02-02 19:16:07 +0700

    'исправление и добавления функции get_print'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from func import format_operation
-from utils import data
-
-for i in range(5):
-    print(format_operation(data[i]))
-    print('')
+from func import format_operation, sorted_list
+def get_print():
+    for i in range(5):
+        print(format_operation(sorted_list[i]))
+        print('')
+    return
+get_print()
